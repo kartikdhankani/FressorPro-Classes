b4933ed7cd82ec452fec16106f8fbdbf8dc4a5d2
kartikdhankani <kartikdhankani19@gmail.com>
2025-05-25 16:46:46 +0530

    Third

--- Class04(250525).py
+++ Class04(250525).py
@@ -1,1 +1,16 @@
 print('Hello World!')
+# Date - 25.05.2025 
+#Day - Sunday
+# Time - 10:00 AM
+
+#list function using "[ ] " brackets
+# You can have any data like strings or integers in a list
+# stocks - AAPL, AMZN, CIPLA, SBIN
+stocks = ['AAPL', 'AMZN', 'CIPLA', 'SBIN']
+# indexing = [ 0,1,2,3]
+a = [22,33,44,55]
+#a[0] will give you first element of the list, and there on. 
+# -1 will give last data, -2 will give last second data and there on.
+a[1]
+a[-1] # will give you last element of the list
+a[-1]
