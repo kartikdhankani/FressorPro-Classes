3cb45d2035852e9f46404f6e677b758f3bb92247
kartikdhankani <kartikdhankani19@gmail.com>
2025-06-01 17:24:24 +0530

    Third Update

--- Class06(010625).py
+++ Class06(010625).py
@@ -67,11 +67,47 @@ l5=[]
 for k in range(100) :  # range(100) generates numbers from 0 to 99
     if k%2 == 0: # Check if the number is even
     l5.append(k) # Append the even number to the list
- print(k) 
+ print('k') 
 
 #type 3 loop - Use a range function to excess elements of a list
 l6 = [55,66,77,88,99,100]
 for i in range(len(l6)):
     print(l6[i]) #This will print each element of the list l6
+
+
+#Practice Exercise 1
+l7 = [4,2,6,7,55,45,67]
+# WE need to find the largest number in the list l7
+largest = l7[0]  # Assume the first element is the largest
+for i in l7:
+    if i > largest:  # If the current element is larger than the largest found so far
+        largest = i  # Update the largest number
+print('Largest number in the list is', largest)
+
+# To find the smallest number in the list l7
+smallest = l7[0]  # Assume the first element is the smallest
+for i in l7:
+    if i < smallest:  # If the current element is smaller than the smallest found so far
+        smallest = i  # Update the smallest number
+print('Smallest number in the list is', smallest)
+
+
+#Practice Exercise 2
+# We have price and volume, we need to calculate VWAP 
+# FOrmula = [ (price * volume) / total volume ]
+
+price = [100,105,110]
+volume = [200,150,300]
+
+num=0 #Have a starting point for numerator
+den=0 # Hav e starting point for denominator
+
+for k in range(len(price)):
+    price_volume = price[k] * volume[k]  # Calculate price * volume for each index
+    num= num + price_volume  # Sum up the numerator
+    den = den + volume[k]  # Sum up the total volume
+vwap = num / den  # Calculate VWAP
+print('VWAP is', vwap)
+
 
 
