cc83392bd7946db9a4dcabc4260f2e9cd5816dbc
kartikdhankani <kartikdhankani19@gmail.com>
2025-06-21 16:48:35 +0530

    Final

---
+++ Class11(220625).py
@@ -0,0 +1,19 @@
+#Date - 21.06.2025
+#Managing text files
+# We can only record strings in text files.
+
+#a='tsla'
+#f1=open('demo1.txt','w')
+#f1.write(a)
+#f1.close()
+
+#f2=open('demo1.txt','r')
+#d=fr2.read()
+#dprint(d)
+#fr.close()
+
+# There are multiple modes -
+# r mode - Read mode
+# w mode - Erase the previous data and write new data
+# a mode - append mode - It will add data but not delete previous one
+
