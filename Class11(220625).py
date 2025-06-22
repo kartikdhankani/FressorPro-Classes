cf14d4f11aac3b5ec272a11cd2901e1006c82c33
kartikdhankani <kartikdhankani19@gmail.com>
2025-06-22 16:33:47 +0530

    Final

--- Class11(220625).py
+++ Class11(220625).py
@@ -8,12 +8,16 @@
 #f1.close()
 
 #f2=open('demo1.txt','r')
-#d=fr2.read()
-#dprint(d)
-#fr.close()
+#d=f2.read()
+#print(d)
+#f2.close()
 
 # There are multiple modes -
 # r mode - Read mode
 # w mode - Erase the previous data and write new data
 # a mode - append mode - It will add data but not delete previous one
+
+#Sometimes you need to take files from other directories; in this case, put entire address of the file, including folder name.
+
+#You have to specify complete address of file - User, Desktop,,Terminal, directory
 
