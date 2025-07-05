1b029b17699302ee97363c1dc96be8f43265ff33
kartikdhankani <kartikdhankani19@gmail.com>
2025-07-05 17:53:43 +0530

    final

---
+++ .vscode/Class 15(05072025).py
@@ -0,0 +1,15 @@
+# Importing panda
+
+import pandas as pd
+
+d1={'a':[1,2,3,4],'b':[5,6,7,8]}
+
+stock=[{"name":"AAPL","price":150.0,"volume":1000},
+       {"name":"GOOGL","price":2800.0,"volume":500},    
+       {"name":"AMZN","price":3400.0,"volume":300}]
+
+df3=pd.DataFrame(stock)
+
+
+
+
